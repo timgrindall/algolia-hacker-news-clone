9e8551abde6e156355d27ef56f825c314361fc53
Timothy Grindall <tim.grindall@gmail.com>
2019-09-06 12:14:58 -0700

    Updated link in comments

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -55,9 +55,7 @@ class Item extends Component {
               <li>
                 <a href={data.link} title={data.date}>{this.getDate(data.date)}</a>
               </li>
-              <li>
-                <a href={data.link} title="See original post at HN">{this.props.comment ? "" : data.comments + " "}{data.comments !== 1 ? "comments" : "comment"}</a>
-              </li>
+              { this.props.comment ? <li><a href={"https://news.ycombinator.com/item?id=" + data.story_id} title="See original post at HN">{this.props.comment ? "" : data.comments + " "}{data.comments !== 1 ? "comments" : "comment"}</a></li> : null}
               {this.props.comment || this.props.story_text ? null : <li className="link"><a href={data.link}>({data.link})</a></li>}
             </ul>
             {this.props.story_text !== null ? <div className="comment"><RawHTML>{this.props.story_text}</RawHTML></div> : (this.props.comment ? <div className="comment"><RawHTML>{data.comment_text}</RawHTML></div> : null)}
--- src/Components/List.js
+++ src/Components/List.js
@@ -13,7 +13,7 @@ class List extends Component {
 		const hits = this.props.data.hits
 
 		const items = hits.map((itemData) => {
-			return <Item key={itemData.objectID} data={{title: (itemData._tags[0] == "comment" ? (this.props.highlight ? itemData._highlightResult.story_title.value : itemData.story_title) : (this.props.highlight ? itemData._highlightResult.title.value : itemData.title)), points: itemData.points, comments: itemData.num_comments, user: itemData.author, date: itemData.created_at, link: ( this.props.comment ? itemData.story_url : itemData.url ), comment_text: itemData.comment_text}} story_text={itemData._highlightResult.story_text ? itemData._highlightResult.story_text.value : null} comment={itemData._tags[0] == "story" ? false : true}/>
+			return <Item key={itemData.objectID} data={{title: (itemData._tags[0] == "comment" ? (this.props.highlight ? itemData._highlightResult.story_title.value : itemData.story_title) : (this.props.highlight ? itemData._highlightResult.title.value : itemData.title)), points: itemData.points, comments: itemData.num_comments, user: itemData.author, date: itemData.created_at, link: ( this.props.comment ? itemData.story_url : itemData.url ), comment_text: itemData.comment_text, story_id: (this.props.comment ? itemData.story_id : null)}} story_text={itemData._highlightResult.story_text ? itemData._highlightResult.story_text.value : null} comment={itemData._tags[0] == "story" ? false : true}/>
 		})
 
 		return (
